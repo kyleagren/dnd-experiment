060f31069eeb3b3612ba598316db98509129d44e
Kyle Agren <kyleagren@gmail.com>
2025-02-04 09:56:49 -0700

    Add a view changer for homework helper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeworkHelperComponent } from './homework-helper/homework-helper.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeworkHelperComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/homework-helper/homework-helper.component.ts
+++ src/app/homework-helper/homework-helper.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import {
+  MatButtonToggleModule,
+  MatButtonToggleGroup,
+} from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-homework-helper',
   standalone: true,
-  imports: [],
+  imports: [MatButtonToggleModule, MatButtonToggleGroup],
   templateUrl: './homework-helper.component.html',
-  styleUrl: './homework-helper.component.scss'
+  styleUrl: './homework-helper.component.scss',
 })
-export class HomeworkHelperComponent {
-
-}
+export class HomeworkHelperComponent {}
